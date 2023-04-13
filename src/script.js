8e8dca78deedca93ba3da233229f499698e1be17
suarestalles <tallessuares48@gmail.com>
2023-04-13 13:22:40 -0300

    First commit

---
+++ src/script.js
@@ -0,0 +1,42 @@
+var $btnSalvar = document.getElementById("btnSalvar");
+var $btnCancelar = document.getElementById("btnCancelar");
+var $planoSim = document.getElementById("planoSim");
+var $planoNao = document.getElementById("planoNao");
+var $dadosPlanoSaude = document.getElementById("dadosPlanoSaude");
+var $htmlDadosPlanoSaude =  "<label for='matriculaPlano' class='labelMatriculaPlano' >Matrícula do Plano:"  + 
+                            "<input type='number'  name='matriculaPlano' id='matriculaPlano' placeholder='Ex.: 20210725020' required> " + 
+                            "</label>" + 
+                            "<label for='vencimentoCarteirinha' class='labelVencimentoCarteirinha' >Vencimento da Carteirinha" + 
+                            "<input type='date'  name='vencimentoCarteirinha' id='vencimentoCarteirinha' required>" +
+                            "</label>";
+var $inputs = document.getElementsByClassName("input");
+var $inputsRadio = document.getElementsByClassName("inputRadio");
+
+$dadosPlanoSaude.innerHTML = '';
+
+$planoSim.addEventListener("click", mostrarCamposPlanoSaude)
+$planoNao.addEventListener("click", esconderCamposPlanoSaude)
+$btnCancelar.addEventListener("click", cancelar);
+
+$btnSalvar.addEventListener("click", salvar);
+
+function salvar() {
+    alert("Prontuário número " + document.getElementById("prontuario").value + " salvo com sucesso!");
+}
+
+function mostrarCamposPlanoSaude() {
+    $dadosPlanoSaude.innerHTML = $htmlDadosPlanoSaude;
+}
+
+function esconderCamposPlanoSaude() {
+    $dadosPlanoSaude.innerHTML = '';
+}
+
+function cancelar() {
+    for (var i=0;i<$inputs.length;i++) {
+        $inputs[i].value = '';
+    }
+    for (var j=0;j<$inputsRadio.length;j++) {
+        $inputsRadio[j].checked = false;
+    }
+}
