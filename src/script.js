da0e558b0c1077a06bc4704a1aa4b425ca7bcc0a
Talles Suares <115495058+suarestalles@users.noreply.github.com>
2023-04-16 12:30:29 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,30 +1,8 @@
-var $btnSalvar = document.getElementById("btnSalvar");
 var $btnCancelar = document.getElementById("btnCancelar");
-var $planoSim = document.getElementById("planoSim");
-var $planoNao = document.getElementById("planoNao");
-var $dadosPlanoSaude = document.getElementById("dadosPlanoSaude");
-var $htmlDadosPlanoSaude =  "<label for='matriculaPlano' class='labelMatriculaPlano' >Matrícula do Plano:"  + 
-                            "<input type='number'  name='matriculaPlano' id='matriculaPlano' placeholder='Ex.: 20210725020' required> " + 
-                            "</label>" + 
-                            "<label for='vencimentoCarteirinha' class='labelVencimentoCarteirinha' >Vencimento da Carteirinha" + 
-                            "<input type='date'  name='vencimentoCarteirinha' id='vencimentoCarteirinha' required>" +
-                            "</label>";
 var $inputs = document.getElementsByClassName("input");
 var $inputsRadio = document.getElementsByClassName("inputRadio");
 
-$dadosPlanoSaude.innerHTML = '';
-
-$planoSim.addEventListener("click", mostrarCamposPlanoSaude)
-$planoNao.addEventListener("click", esconderCamposPlanoSaude)
 $btnCancelar.addEventListener("click", cancelar);
-
-function mostrarCamposPlanoSaude() {
-    $dadosPlanoSaude.innerHTML = $htmlDadosPlanoSaude;
-}
-
-function esconderCamposPlanoSaude() {
-    $dadosPlanoSaude.innerHTML = '';
-}
 
 function cancelar() {
     for (var i=0;i<$inputs.length;i++) {
